9466970c19a0e75a95db7da027a5871458592034
Sandro Sena <sandro340@hotmail.com>
2018-07-05 00:11:01 -0300

    Preloader

--- js/main.js
+++ js/main.js
@@ -1,80 +1,45 @@
-//~ Skillbar 
+//~ Skillbar
 
-$(document).ready(function(){
-    $('.skillbar').each(function(){
-        $(this).find('.skillbar-bar').animate({
-            width:$(this).attr('data-percent')
-        },9000);
-    });
+$(document).ready(function() {
+  $(".skillbar").each(function() {
+    $(this)
+      .find(".skillbar-bar")
+      .animate(
+        {
+          width: $(this).attr("data-percent")
+        },
+        9000
+      );
+  });
 });
 
 //~ Visibility
 
-Visibility.onVisible(function(){
-	setTimeout(function () {
-		$(".header-img").addClass("animated zoomIn");
-    }, 400);
-    setTimeout(function () {
-		$(".header-h1").addClass("animated zoomIn");
-    }, 900);
-    setTimeout(function () {
-		$(".header-span").addClass("animated zoomIn");
-    }, 1300);
-    setTimeout(() => {
-        $(".footer-p").addClass("animated infinite shake");
-    }, 10000)
+Visibility.onVisible(function() {
+  setTimeout(function() {
+    $(".header-img").addClass("animated zoomIn");
+  }, 400);
+  setTimeout(function() {
+    $(".header-h1").addClass("animated zoomIn");
+  }, 900);
+  setTimeout(function() {
+    $(".header-span").addClass("animated zoomIn");
+  }, 1300);
+  setTimeout(() => {
+    $(".footer-p").addClass("animated infinite shake");
+  }, 10000);
 });
 
-//~ Formulario
+//~ Spinner (Preloader)
 
-$('.form-php').on('submit', function() {
-	var emailContato = "sandro340@hotmail.com"; // Escreva aqui o seu e-mail
-
-	var that = $(this),
-			url = that.attr('action'),
-			type = that.attr('method'),
-			data = {};
-	
-	that.find('[name]').each(function(index, value) {
-		var that = $(this),
-				name = that.attr('name'),
-				value = that.val();
-				
-		data[name] = value;
-	});
-	
-	$.ajax({
-		url: url,
-		type: type,
-		data: data,
-		success: function(response) {
-		
-			if( $('[name="leaveblank"]').val().length != 0 ) {
-				$('.form-php').html("<div id='form-erro'></div>");
-				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
-				.hide()
-				.fadeIn(1500, function() {
-				$('#form-erro');
-				});
-			} else {
-			
-				$('.form-php').html("<div id='form-send'></div>");
-				$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
-				.hide()
-				.fadeIn(1500, function() {
-				$('#form-send');
-				});
-			};
-		},
-		error: function(response) {
-			$('.form-php').html("<div id='form-erro'></div>");
-			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
-			.hide()
-			.fadeIn(1500, function() {
-			$('#form-erro');  
-		});
-		}
-	});
-	
-	return false;
+$(document).ready(function() {
+  //Preloader
+  $(window).on("load", function() {
+    preloaderFadeOutTime = 500;
+    function hidePreloader() {
+      var preloader = $(".spinner-wrapper");
+      preloader.fadeOut(preloaderFadeOutTime);
+    }
+    hidePreloader();
+  });
 });
